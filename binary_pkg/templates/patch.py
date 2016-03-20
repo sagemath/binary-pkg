3b40eb50a65119640601007d623c3b8465a4b73a
Volker Braun <vbraun.name@gmail.com>
2016-03-20 12:13:40 +0100

    Faster binary patching

--- binary_pkg/templates/patch.py
+++ binary_pkg/templates/patch.py
@@ -1,20 +1,25 @@
 
-class FilePatch(object):
+
+class FileABC(object):
 
     def __init__(self, search_and_replace, filename):
         self.search = search_and_replace.search
         self.replace = search_and_replace.replace
         self.filename = filename
         self.st = os.stat(filename)
-        with open(self.filename, 'rb') as f:
-            self.content = f.read()
 
-    def substitute(self):
+    def save(self):
         """
-        Perform the string substitution, changing the file length
+        Preserve timestamps
         """
-        self.content = self.content.replace(self.search, self.replace)
-        return self
+        os.utime(self.filename, (self.st.st_atime, self.st.st_mtime))
+
+        
+class FileBinaryPatch(FileABC):
+
+    def __init__(self, search_and_replace, filename):
+        super(FileBinaryPatch, self).__init__(search_and_replace, filename)
+        self.fh = open(filename, "r+b")
         
     def patch(self, start, end):
         """
@@ -27,19 +32,56 @@ class FilePatch(object):
             start(int): The start position of the string to replace
             end(int): The position of the terminating null
         """
-        assert self.content[end:end+1] == b'\0'
-        region = self.content[start:end]
+        self.fh.seek(start)
+        region = self.fh.read(end - start + 1)
+        assert region[-1] == b'\0'
         substituted = region.replace(self.search, self.replace)
         substituted+= b'\0' * (end - start - len(substituted))
-        self.content = self.content[:start] + substituted + self.content[end:]
+        self.fh.seek(start)
+        self.fh.write(substituted)
         return self
 
     def save(self):
-        with open(self.filename, 'wb') as f:
-            f.write(self.content)
-        os.utime(self.filename, (self.st.st_atime, self.st.st_mtime))
+        self.fh.close()
+        super(FileBinaryPatch, self).save()
 
+
+class FileTextSearchReplace(FileABC):
+
+    def __init__(self, search_and_replace, filename):
+        super(FileTextSearchReplace, self).__init__(search_and_replace, filename)
+        with open(self.filename, 'rb') as f:
+            self.content = f.read()
         
+    def substitute(self):
+        """
+        Perform the string substitution, changing the file length
+        """
+        self.content = self.content.replace(self.search, self.replace)
+        return self
+        
+    def save(self):
+        with open(self.filename, 'wb') as f:
+            f.write(self.content)        
+        super(FileTextSearchReplace, self).save()
+
+
+class FileProxy(object):
+
+    def __init__(self, search_and_replace, filename):
+        self.search_and_replace = search_and_replace
+        self.filename = filename
+
+    def substitute(self):
+        return FileTextSearchReplace(
+            self.search_and_replace, self.filename
+        ).substitute()
+        
+    def patch(self, start, end):
+        return FileBinaryPatch(
+            self.search_and_replace, self.filename
+        ).patch(start, end)
+
         
 class SearchAndReplace(object):
 
@@ -55,9 +97,4 @@ class SearchAndReplace(object):
     def __call__(self, filename):
         filename = os.path.join(self.root_path, filename)
         print('patching {0}'.format(filename))
-        return FilePatch(self, filename)
-    
-
-
-        
-        
+        return FileProxy(self, filename)
